c182e9468638a29e5ee17e6f7e5a5f66ce79a35d
Joseph <akinolajoseph422@gmail.com>
2021-05-14 03:14:00 +0100

    Addition of countdown timer functions

--- app/src/main/java/com/borat/nsneq/ExamActivity.java
+++ app/src/main/java/com/borat/nsneq/ExamActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -16,10 +17,12 @@ import android.widget.Toast;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 public class ExamActivity extends AppCompatActivity {
 
     public static final String EXTRA_SCORE = "extraScore";
+    private static final long COUNTDOWN_IN_MILLIS = 30000;
 
     private TextView textViewQuestion;
     private TextView textViewScore;
@@ -30,7 +33,12 @@ public class ExamActivity extends AppCompatActivity {
     private RadioButton rb2;
     private RadioButton rb3;
     private Button buttonConfirmNext;
+
     private ColorStateList textColorDefaultRb;
+    private ColorStateList getTextColorDefaultCd;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
 
     private List<Question> questionList;
     private int questionCounter;
@@ -58,6 +66,7 @@ public class ExamActivity extends AppCompatActivity {
         buttonConfirmNext = findViewById(R.id.button_confirm_next);
 
         textColorDefaultRb =rb1.getTextColors();
+        getTextColorDefaultCd = textViewCountDown.getTextColors();
 
         ExamDbHelper dbHelper = new ExamDbHelper(this);
         questionList = dbHelper.getAllQuestions();
@@ -99,11 +108,48 @@ public class ExamActivity extends AppCompatActivity {
             textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
             answered = false;
             buttonConfirmNext.setText("Confirm");
+
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
         }else {
             finishExam();
         }
 
     }
+
+    private void startCountDown(){
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+
+                timeLeftInMillis = 0;
+                updateCountDownText();
+
+            }
+        }.start();
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int)(timeLeftInMillis /1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+
+        String timeformatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        textViewCountDown.setText(timeformatted);
+
+        if (timeLeftInMillis < 10000){
+            textViewCountDown.setTextColor(Color.RED);
+        }else{
+            textViewCountDown.setTextColor(getTextColorDefaultCd);
+        }
+    }
+
     private void checkAnswer(){
         answered = true;
         RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
