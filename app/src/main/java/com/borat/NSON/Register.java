11ab440abaf9330326e1be1184a48ce5277abc39
Joseph <akinolajoseph422@gmail.com>
2021-05-22 07:37:32 +0100

    Firebase sync in Register.java and login.java with implementation of login access

--- app/src/main/java/com/borat/NSON/Register.java
+++ app/src/main/java/com/borat/NSON/Register.java
@@ -1,13 +1,22 @@
 package com.borat.NSON;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
@@ -26,13 +35,69 @@ public class Register extends AppCompatActivity {
         mFullName = findViewById(R.id.fullName);
         mEmail = findViewById(R.id.Email);
         mPassword = findViewById(R.id.password);
-        mPassword =findViewById(R.id.phone);
+        mPhone =findViewById(R.id.phone);
         mRegisterBtn = findViewById(R.id.RegisterBtn);
         mLoginBtn = findViewById(R.id.CreateText);
 
 
         fAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
+
+        //if registered send user to main activity
+        if (fAuth.getCurrentUser() != null ){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+
+        }
+
+
+        mRegisterBtn.setOnClickListener(v -> {
+            String email = mEmail.getText().toString().trim();
+            String password = mPassword.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)){
+                mEmail.setError("Email is Required. ");
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)){
+                mPassword.setError("Password is Required. ");
+                return;
+            }
+
+            if (password.length() < 8){
+                mPassword.setError("Password Must be 8 or more characters Long");
+                return;
+            }
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            //Register the user in firebase
+
+
+            fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(Register.this, "Account Created", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                    }else {
+                        Toast.makeText(Register.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+
+                    }
+                }
+            });
+        });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+
+            }
+        });
 
     }
 }
