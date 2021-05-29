bb676050028ca07768be076a7d2a9b492f4061d1
Joseph <akinolajoseph422@gmail.com>
2021-05-29 11:55:11 +0100

    fixed multidex but. just added to the build gradle config multiDexEnabled true

--- app/src/main/java/com/borat/NSON/ContactUs.java
+++ app/src/main/java/com/borat/NSON/ContactUs.java
@@ -3,12 +3,32 @@ package com.borat.NSON;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 public class ContactUs extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
-    }
+
+
+        }
+
 }
